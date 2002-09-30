347b28de9dcda6cf6fdde4284ccd34636abb6b0e
Gwynne Raskind <gwynne@php.net>
2002-09-30 03:46:00 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_1'.
    git-svn-id: http://svn.php.net/repository/pear/packages/SQL_Parser/tags/RELEASE_0_1@97445 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/select.sql
+++
@@ -1,7 +0,0 @@
-select * from dog where cat <> 4;
-select legs, hairy from dog;
-select max(length) from dog;
-SELECT COUNT(DISTINCT country) FROM publishers;
-select one, two from hairy where two <> 4 and one = 2;
-select one, two from hairy where two <> 4 and one = 2 order by two;
-select one, two from hairy where two <> 4 and one = 2 limit 4 order by two ascending, dog descending;
