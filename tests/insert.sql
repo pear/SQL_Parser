622068b11ef19648049bcbd67d29d28ddba28dc3
Gwynne Raskind <gwynne@php.net>
2002-12-18 06:43:13 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_2'.
    git-svn-id: http://svn.php.net/repository/pear/packages/SQL_Parser/tags/RELEASE_0_2@108408 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/insert.sql
+++
@@ -1,2 +0,0 @@
-insert into dogmeat ('horse', 'hair') values (2, 4);
-inSERT into dogmeat (horse, hair) values (2, 4);
--- tests/update.sql
+++
@@ -1,7 +0,0 @@
-update dogmeat set horse=2 dog='forty' where moose <> 'howdydoo';
-update dogmeat set horse=2, dog='forty' where moose != 'howdydoo';
-update dogmeat set horse=2, dog='forty' where moose <> 'howdydoo';
-update table1 set col=1 where not col = 2;
-update table2 set col=1 where col > 2 and col <> 4;
-update table2 set col=1 where col > 2 and col <> 4 or dog="Hello";
-update table3 set col=1 where col > 2 and col < 30;
