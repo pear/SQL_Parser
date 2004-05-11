b1602ee2f681f76950e61bc3aa085d639658a7ea
Gwynne Raskind <gwynne@php.net>
2004-05-11 05:16:31 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_5'.
    git-svn-id: http://svn.php.net/repository/pear/packages/SQL_Parser/tags/RELEASE_0_5@158343 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/delete.sql
+++
@@ -1,5 +0,0 @@
-delete from dog where cat = 4 and horse <> "dead meat" or mouse = 'furry';
-delete from;
-delete from cat;
-delete from where cat = 53;
-delete from dog where mouse is happy;
--- tests/insert.sql
+++
@@ -1,3 +0,0 @@
-insert into dogmeat ('horse', 'hair') values (2, 4);
-inSERT into dogmeat (horse, hair) values (2, 4);
-INSERT INTO mytable (foo, bar, baz) VALUES (NOW(), 1, 'text');
