bedc7e73cfaa33e907c9c02316a3e9ae86a1022c
Daniel O'Connor <clockwerx@php.net>
2010-02-12 05:56:53 +0000

    Fixed PHPUnit 3.4 compatibility
    git-svn-id: http://svn.php.net/repository/pear/packages/SQL_Parser/trunk@294993 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,7 +4,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 }
 chdir(dirname(__FILE__) . '/../');
 
-require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once './Parser.php';
 
